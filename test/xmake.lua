bd174dafb8ac51f5e8e69ff77ab23525756a5988
Zhe Feng <zhefeng7110@outlook.com>
2026-04-17 18:04:11 +0800

    Refactor: Provide detailed tests for XMake

---
+++ test/xmake.lua
@@ -0,0 +1,18 @@
+add_requires("gtest v1.17.0", {configs = {main = true, gmock = false}})
+
+target("cpp_pipe_operator_helper_test", function ()
+    set_kind("binary")
+    set_languages("cxx20")
+    set_policy("build.c++.modules", true)
+
+    if has_config("vs") then
+        add_cxxflags("/Zc:__cplusplus")
+    end
+
+    add_packages("gtest")
+    add_deps("cpp_pipe_operator_helper")
+
+    add_defines("TEST_USE_MODULE")
+    add_files("*.cpp", "pipe_operators_tests/*.cpp")
+    add_includedirs("./")
+end)
--- xmake.lua
+++ xmake.lua
@@ -4,48 +4,24 @@ local project_name = "cpp_pipe_operator_helper"
 set_project(project_name)
 set_version("1.0.0")
 
-option(project_name .. "-use-modules", function ()
+option("modules", function ()
     set_description("Use C++20 modules for library `" .. project_name .. "`.")
     set_default(false)
 end)
 
-option(project_name .. "-build-shared", function ()
-    set_description("Build cpp_pipe_operator_helper as a shared library if " ..
-        "`cpp_pipe_operator_helper-use-modules` is true."
-    )
-    set_default(false)
-end)
-
-option(project_name .. "-enable-test", function ()
-    set_description("Test project `" .. project_name .. "` could runing with XMake " ..
-        "or not. If you need running unit test, please use CMake with " ..
-        "`-DCPP_PIPE_OPERATOR_HELPER_BUILD_TESTS=ON`"
-    )
+option("test", function ()
+    set_description("Build tests for " .. project_name .. ".")
     set_default(false)
 end)
 
 target(project_name, function ()
     on_config(function (target)
-        if get_config(project_name .. "-use-modules") then
-            if get_config(project_name .. "-build-shared") then
-                print("[%s]: provide modules with shared library.", project_name)
-                target:set("kind", "shared")
-            else
-                print("[%s]: provide modules with static library.", project_name)
-                target:set("kind", "static")
-            end
-
+        if get_config("modules") then
+            print("[%s]: provide modules.", project_name)
+            target:set("kind", "moduleonly")
             target:add("files", "pipe_operator_helper.cppm", {public = true})
         else
             print("[%s]: provide headers.", project_name)
-            if get_config(project_name .. "-build-shared") then
-                local warn_str = format("${yellow}WARNING${clear}: [%s]: Config `%s-build-shared` " ..
-                    "ignored because `%s-use-modules` was false. Set `%s-build-shared` to " ..
-                    "false to avoid this warning.", project_name, project_name, project_name,
-                    project_name
-                )
-                cprint(warn_str)
-            end
             target:set("kind", "headeronly")
         end
     end)
@@ -53,13 +29,6 @@ target(project_name, function ()
     add_includedirs("include", {public = true})
 end)
 
-if get_config(project_name .. "-enable-test") then
-    target(project_name .. "_test", function ()
-        set_kind("binary")
-
-        add_defines("TEST_USE_MODULE")
-        add_files("test/xmake_test/xmake_test.cpp")
-        set_languages("cxx20")
-        add_deps(project_name)
-    end)
+if get_config("test") then
+    includes("test")
 end
